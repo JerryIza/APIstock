7a1887709470afbc5c6167f0aede98d4468275cc
Jerry Izaguirre <53665133+JerryIza@users.noreply.github.com>
2022-07-05 13:22:56 -0500

    Test 2 try this again

--- app/src/main/java/com/example/composetdaapp/utils/MySharedPref.kt
+++ app/src/main/java/com/example/composetdaapp/utils/MySharedPref.kt
@@ -18,7 +18,7 @@ const val SOCKET_CREDENTIALS: String = "SOCKET_CREDENTIALS"
 const val ACCOUNT_NUMBER: String = "ACCOUNT_VALUE"
 const val USER_ID: String = "USER_ID"
 const val SPINNER_POS: String = "SPINNER_POS"
-//TEST 2
+//TEST 2 let's try this again
 @Singleton
 class MyPreference @Inject constructor(@ApplicationContext context: Context) {
 
